cfce8f47d1f48526135761466e8bd6cf3b6e7149
Aaron Santos <2302042@utrivieramaya.edu.mx>
2024-11-28 08:20:42 -0500

    feat: Creación del HTML del AboutUs.vue
    Se crea toda la estructura HTML del componente,
    comentando todo excepto el hero section y partiendo
    de ahí, se agregan los estilos CSS

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,11 +1,228 @@
 <template>
-    <section id="aboutus">
-        <h1>About Us</h1>
+    <!-- ! Main: Hero content -->
+    <section class="hero-contnt-main">
+        <img class="hero-img-backgrd" 
+        src="../assets/img-aboutUs/img-hero-aboutUs.jpg" 
+        alt="People training image"
+        >
+        <div class="hero-txt-contnt">
+            <h1>Discover the Future of Gym Management and Personalized Fitness</h1>
+            <p> At Time Fit, we're transforming the way gyms operate and how 
+                users achieve their fitness goals. With a unique blend of advanced 
+                technology, personalized routines, and a strong community focus, we’re 
+                setting a new standard in the fitness industry.
+            </p>
+        </div>
     </section>
+
+    <!-- ! Main: Info text and imgs about us-->
+    <!-- <section class="info-txt-img-contnt-main">
+
+        <div class="info-txt-img-contnt">
+            <img src="../assets/img-aboutUs/img-team-about-1.jpg" alt="Time Fit members">
+            <div>
+                <h2>Who are we?</h2>
+
+                <p>At Time Fit, we are a team passionate about technology 
+                    and fitness, committed to transforming the experience of 
+                    gyms and their users through innovative and accessible solutions.
+                    We believe in the power of a platform that simplifies gym 
+                    management while enhancing each client’s experience with 
+                    personalized tools and progress tracking.</p>
+
+                <p>Inspired by the real needs of users and administrators, 
+                    we have developed a comprehensive platform that combines 
+                    process automation with personalized routine creation and 
+                    an active community space to keep users motivated.</p>
+            </div>
+        </div>
+
+        <div class="info-txt-img-contnt">
+            <img src="../assets/img-aboutUs/img-vision-about-2.png" alt="Trainer talking to a client">
+            <div>
+                <h2>Vision</h2>
+                <p>At Time Fit, our vision is to revolutionize the fitness 
+                    industry through advanced technology and personalization.
+                    We see ourselves as the leading platform in gym management,
+                    integrating wearable devices and cutting-edge technologies to 
+                    deliver a fitness experience that adapts in real-time to each 
+                    user's needs. We envision a world where both administrators 
+                    and users have instant access to relevant information, 
+                    personalized routines, and performance metrics—all in a
+                    single platform. </p>
+            </div>
+        </div>
+
+        <div class="info-txt-img-contnt">
+            <img src="../assets/img-aboutUs/img-mission-about-3.png" alt="People in the Gym">
+            <div>
+                <h2>Mission</h2>
+                <p>At Time Fit, our mission is to revolutionize gym management
+                    and user experience through an accessible, intuitive, and 
+                    personalized platform. We believe in the importance of 
+                    simplifying daily gym operations, allowing administrators
+                    to manage their activities efficiently and effortlessly. 
+                    At the same time, we aim to provide each user with a unique 
+                    experience, with personalized tools that adapt to their 
+                    individual goals, motivating them to reach their fitness
+                    objectives.</p>
+            </div>
+        </div>
+
+    </section> -->
+
+    <!-- ! Main: Team members - Cards -->
+    <!-- <header class="title-cards">
+        <h1>Team Members</h1>
+    </header>
+
+    <section class="team-membs-contnt-main">
+        <div class="team-membs-contnt">
+            
+            Target 1
+            <article>
+                
+                <figure>
+                    <img src="../assets/img-aboutUs/Diego-foto-card.jpg" alt="Diego Balbuena picture">
+                </figure>
+                <div class="txt-team-membs-contnt">
+                    <p>Balbuena Caballero Diego</p>
+                    <p>UX/UI Designer</p>
+                </div>
+                
+            </article>
+            
+            Target 2
+            <article>
+
+                <figure>
+                    <img src="../assets/img-aboutUs/Enrique-foto-card.jpg" alt="Enrique Castillo picture">
+                </figure>
+                <div class="txt-team-membs-contnt">
+                    <p>Castillo Rodríguez Enrique</p>
+                    <p>CEO project</p>
+                </div>
+
+            </article>
+
+            Target 3
+            <article>
+
+                <figure>
+                    <img src="../assets/img-aboutUs/Yair-foto-card.jpg" alt="Yair Gamaliel picture">
+                </figure>
+                <div class="txt-team-membs-contnt">
+                    <p>Guzmán Pérez Yair Gamaliel</p>
+                    <p>Full Stack Developer</p>
+                </div>
+
+            </article>
+
+            Target 4
+            <article>
+
+                <figure>
+                    <img src="../assets/img-aboutUs/Cesar-foto-card.jpg" alt="Cesar Sanchez picture">
+                </figure>
+                <div class="txt-team-membs-contnt">
+                    <p>Sánchez Trejo Cesar David</p>
+                    <p>Network Architect</p>
+                </div>
+
+            </article>
+
+            Target 5
+            <article>
+
+                <figure>
+                    <img src="../assets/img-aboutUs/Aaron-foto-card.jpg" alt="Aarón Santos picture">
+                </figure>
+                <div class="txt-team-membs-contnt">
+                    <p>Santos Absalón Aarón de Jesús</p>
+                    <p>UX/UI Designer</p>
+                </div>
+
+            </article>
+
+        </div>
+    </section> -->
+
+     <!-- ! Main: Asked questions - Accordion -->
+    <!-- <section class="ask-accor-contnt-main">
+        <h2>Frequently Asked Question</h2>
+
+        <details class="ask-acorr-quest-contnt">
+            <summary style="color: greenyellow;">
+                How does Time Fit help gyms improve client retention?
+            </summary>
+            <p> 
+                Allows gyms to offer a personalized experience for each client, 
+                with workout routines and diet plans tailored to their goals. 
+                It also includes an integrated community that motivates users 
+                to stay active and committed to their objectives, which helps 
+                improve long-term client retention.
+            </p>
+        </details>
+
+        <details class="ask-acorr-quest-contnt">
+            <summary style="color: greenyellow;">
+                How easy is it to integrate Time Fit with a gym's daily operations?
+            </summary>
+            <p> 
+                Allows gyms to offer a personalized experience for each client, 
+                with workout routines and diet plans tailored to their goals. 
+                It also includes an integrated community that motivates users 
+                to stay active and committed to their objectives, which helps 
+                improve long-term client retention.
+            </p>
+        </details>
+
+        <details class="ask-acorr-quest-contnt">
+            <summary style="color: greenyellow;">
+                How does Time Fit personalize each user's experience?
+            </summary>
+            <p> 
+                Allows gyms to offer a personalized experience for each client, 
+                with workout routines and diet plans tailored to their goals. 
+                It also includes an integrated community that motivates users 
+                to stay active and committed to their objectives, which helps 
+                improve long-term client retention.
+            </p>
+        </details>
+
+        <details class="ask-acorr-quest-contnt">
+            <summary style="color: greenyellow;">
+                Is Time Fit suitable for gyms of all sizes?
+            </summary>
+            <p> 
+                Allows gyms to offer a personalized experience for each client, 
+                with workout routines and diet plans tailored to their goals. 
+                It also includes an integrated community that motivates users 
+                to stay active and committed to their objectives, which helps 
+                improve long-term client retention.
+            </p>
+        </details>
+
+    </section> -->
+
 </template>
 
+
 <style scoped>
-h1 {
-    color: white;
-}
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+
+    p {
+        color: cadetblue;
+    }
+
 </style>
+
+<!-- TERMINAR ESTE COMPONENTE -->
