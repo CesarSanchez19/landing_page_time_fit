c7d6e48f8b3a317463be298e3076129bfa2388c4
Aaron Santos <2302042@utrivieramaya.edu.mx>
2024-11-29 21:05:24 -0500

    feat: Agregar la sección 'Info text and imgs about us' al componente AboutUs.vue
    Se descomentó la sección que explica quienes somos,
    nuestra visión y misión en Time Fit, ademas de agregar
    todos los estilos correspondientes. Ahora es completamente
    responsivo en pantallas mas pequeñas

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -16,11 +16,15 @@
     </section>
 
     <!-- ! Main: Info text and imgs about us-->
-    <!-- <section class="info-txt-img-contnt-main">
+    <section class="info-txt-img-contnt-main">
 
-        <div class="info-txt-img-contnt">
-            <img src="../assets/img-aboutUs/img-team-about-1.jpg" alt="Time Fit members">
-            <div>
+        <!--? Who are we content -->
+        <div class="info-txt-img-contnt info-txt-img-contnt-1">
+
+            <img class="info-img-1" src="../assets/img-aboutUs/img-team-about-1.jpg" 
+            alt="Time Fit members">
+
+            <div class="info-txt-contnt info-txt-contnt-1">
                 <h2>Who are we?</h2>
 
                 <p>At Time Fit, we are a team passionate about technology 
@@ -29,7 +33,7 @@
                     We believe in the power of a platform that simplifies gym 
                     management while enhancing each client’s experience with 
                     personalized tools and progress tracking.</p>
-
+                <br>
                 <p>Inspired by the real needs of users and administrators, 
                     we have developed a comprehensive platform that combines 
                     process automation with personalized routine creation and 
@@ -37,9 +41,13 @@
             </div>
         </div>
 
-        <div class="info-txt-img-contnt">
-            <img src="../assets/img-aboutUs/img-vision-about-2.png" alt="Trainer talking to a client">
-            <div>
+        <!--? Vision content-->
+        <div class="info-txt-img-contnt info-txt-img-contnt-2 revers-row">
+
+            <img class="info-img-2" src="../assets/img-aboutUs/img-vision-about-2.png" 
+            alt="Trainer talking to a client">
+
+            <div class="info-txt-contnt info-txt-contnt-2">
                 <h2>Vision</h2>
                 <p>At Time Fit, our vision is to revolutionize the fitness 
                     industry through advanced technology and personalization.
@@ -53,9 +61,13 @@
             </div>
         </div>
 
-        <div class="info-txt-img-contnt">
-            <img src="../assets/img-aboutUs/img-mission-about-3.png" alt="People in the Gym">
-            <div>
+        <!--? Mission content -->
+        <div class="info-txt-img-contnt info-txt-img-contnt-3">
+
+            <img class="info-img-3" src="../assets/img-aboutUs/img-mission-about-3.png" 
+            alt="People in the Gym">
+
+            <div class="info-txt-contnt info-txt-contnt-3">
                 <h2>Mission</h2>
                 <p>At Time Fit, our mission is to revolutionize gym management
                     and user experience through an accessible, intuitive, and 
@@ -69,7 +81,7 @@
             </div>
         </div>
 
-    </section> -->
+    </section>
 
     <!-- ! Main: Team members - Cards -->
     <!-- <header class="title-cards">
@@ -272,7 +284,161 @@
     }
 
     /* ----- Main: Info text and imgs about us ----- */
+    
+    /* Styles resposive main contain */
+    .info-txt-img-contnt-main {
+        width: 100%;
+        height: auto;
+        margin-bottom: 5em;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+    }
 
+    /* Styles for resposive contain of div's containers */
+    .info-txt-img-contnt {
+        display: flex;
+        color: wheat;
+        height: 100%;
+        padding: 1em;
+        justify-content: center;
+        align-items: center;
+        /* margin-bottom: 2em; */
+    }
+
+    /* Styles images of 'Who are we' and 'Mission' contents */
+    .info-txt-img-contnt-1 img, .info-txt-img-contnt-3 img {
+        display: block;
+        /* max-width: 45em; */
+        width: 51%;
+        /* max-height: 33em; */
+        height: 30%;
+        position: relative;
+        left: 7em;
+        margin-top: 5em;
+        border-radius: 10px;
+    }
+
+    /* Styles for resposnive text contain */
+    .info-txt-contnt {
+        border-radius: 10px;
+        margin: 7em 6em 0 2.6em;
+        background: linear-gradient(180deg, #FF4300 0%, #FF6400 100%);
+        display: flex;
+        flex-direction: column;
+        /* Shadow 1 */
+        box-shadow: 0px 20px 24px -4px rgb(17 24 39 / 0.10), 
+        0px 8px 8px -4px rgb(17 24 39 / 0.04);
+        z-index: 2;
+        padding: 2em;
+    }
+    
+    .info-txt-contnt p {
+        font-size: 1em;
+    }
+
+    .info-txt-contnt-1 {
+        /* position: relative; */
+        max-width: 30em;
+        height: 100%;
+        top: 9em;
+        /* margin-top: 1%; */
+    }
+
+    .info-txt-contnt-1 p {
+        font-size: .9em;
+    }
+
+    .info-txt-contnt-2 {
+        max-width: 30em;
+        /* max-height: 24em; */
+        top: 9.5em;
+        height: 100%;
+        margin: 7em 2.6em 0 6em;
+    }
+
+    /* Changed of direction 'vision' content */
+    .revers-row {
+        flex-direction: row-reverse;
+    }
+
+    .revers-row img {
+        display: block;
+        /* max-width: 45em; */
+        width: 51%;
+        /* max-height: 33em; */
+        height: 30%;
+        position: relative;
+        right: 7em;
+        margin-top: 5em;
+        border-radius: 10px;
+    }
+
+    .info-txt-contnt-3 {
+        max-width: 30em;
+        height: 100%;
+        top: 10.5em;
+        
+    }
+
+
+    .info-txt-contnt h2 {
+        font-size: 1.7em;
+        font-weight: 700;
+        padding-bottom: .7em;
+    }
+
+    /* For screen smaller */
+
+    @media(width < 1256px) {
+
+        /*  */
+        .info-txt-img-contnt-main {
+            width: 100%;
+            height: auto;
+            display: flex;
+            flex-direction: column;
+            /* flex-wrap: wrap; */
+            margin-bottom: 5em;
+            margin-top: 5em;
+        }
+
+        .info-txt-img-contnt {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 0;
+            height: auto;
+            margin-top: 0em;
+            contain: content;
+            border-radius: 10px;
+        }
+        
+        .info-txt-img-contnt img {
+            left: 0;
+            width: 77%;
+            margin-top: 0em;
+            height: 100%;
+            border-radius: 10px 10px 0px 0px;
+        }
+
+        .info-txt-img-contnt-2, .info-txt-img-contnt-3 {
+            margin-top: 2em;
+        }
+
+        /* Texto que aun no se muestra */
+        .info-txt-contnt {
+            margin: 0;
+            max-width: 77%;
+            border-radius: 0px 0px 10px 10px;
+        }
+
+        .info-txt-contnt p {
+            font-size: .9em;
+        }
+    }
+
+    /* ----- Main: Team members section ----- */
 
 
 </style>
